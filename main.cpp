57d2d8e5e3736fbff3a7bbbe04f64135a9465ca9
qunqing166 <1730212106@qq.com>
2025-03-26 19:18:38 +0800

    设置程序只能启动单个实例; 添加icon

--- main.cpp
+++ main.cpp
@@ -4,10 +4,18 @@
 #include <QDir>
 #include <QStandardPaths>
 #include <QTextStream>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    static QSharedMemory* singleApp = new QSharedMemory("CraftTable");
+    if(!singleApp->create(1))
+    {
+        a.quit();
+        return -1;
+    }
 
     QFile file(":/res/style/style.qss");
     if (file.open(QFile::ReadOnly | QFile::Text)) {
